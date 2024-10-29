316a621fa58eca8f5bdea6df5aa168da7d3bcbf5
adixb <aditya.bhattacharjee706@gmail.com>
2024-10-29 14:40:52 +0530

    updated code

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, Expo!</Text>
+
     </View>
   );
 }
